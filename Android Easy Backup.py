4a84af55d89edf7a7beec744f6ec8f5e26236c0f
MrChaosbude <mail@chaosbude.com>
2016-06-16 20:58:38 +0200

    Add Reboot Button and pack it in a labelframe

--- Android Easy Backup.py
+++ Android Easy Backup.py
@@ -14,15 +14,30 @@ class CoreGUI(object):
     def __init__(self,parent):
         self.parent = parent
         self.InitUI()
-        button = Button(self.parent, text="Check Device", command=lambda: self.adb( "devices"))
-        button.grid(column=0, row=0, columnspan=1)
 
     def InitUI(self):
+        self.adb_Main()
         self.text_box = Text(self.parent, wrap='word', height = 6, width=50)
         self.text_box.grid(column=0, row=10, columnspan = 2, sticky='NSWE', padx=5, pady=5)
         sys.stdout = StdoutRedirector(self.text_box)
 
-    def adb(self, *args):
+    def adb_Main(self):
+        labelframe = LabelFrame(self.parent, text="ADB Main function:")
+        labelframe.grid(column=0, row=0, rowspan=3)
+
+        check_device = Button(labelframe, text="Check Device", command=lambda: self.adb("devices"))
+        check_device.pack(padx=2, pady=2)
+
+        reboot = Button(labelframe, text="Reboot Normal", command=lambda: self.adb("reboot"))
+        reboot.pack(padx=2, pady=2)
+
+        reboot_recovery = Button(labelframe, text="Reboot Recovery", command=lambda: self.adb("reboot", "recovery"))
+        reboot_recovery.pack(padx=2, pady=2)
+
+        reboot_bootloader = Button(labelframe, text="Reboot Bootloader", command=lambda: self.adb("reboot","bootloader"))
+        reboot_bootloader.pack(padx=2, pady=2)
+
+    def adb(self, *args):#übergiebt die befehle an adb
         process = subprocess.Popen(['adb.exe', args], stdout=subprocess.PIPE, shell=True)
         print(process.communicate())
         #return x.communicate(stdout)
