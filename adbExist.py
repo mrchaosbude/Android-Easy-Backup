9dc7d74ee0ab898044b21336dfc0929d4b97f0f1
mrchaosbude <r2d2r2d2>
2016-06-18 20:29:16 +0200

    finish the adbexit now checks if adb.exe exist if not ask to download it

--- adbExist.py
+++ adbExist.py
@@ -9,25 +9,28 @@ def exist(filename):
     if os.path.exists(filename):
         print("exist")
     else:
-        #get_zip("http://pick.cetus.uberspace.de/adb/ADB.zip")
-        get_zip("http://forum.xda-developers.com/attachment.php?attachmentid=478154&d=1293906896")
+        show_questnion()
 
-def get_zip(file_url):
+def get_zip():
+    file_url = "http://forum.xda-developers.com/attachment.php?attachmentid=478154&d=1293906896"
     x = wget.download(file_url)
     with zipfile.ZipFile(x, "r") as zip_ref:
         zip_ref.extractall("")
     os.remove(x)
+    root.destroy()
+
 
 def show_questnion():
+    global root
     root = tk.Tk()
     root.title("ADB Not found")
 
-    label = tk.Label(root, text="Can not find adb.exe should I download it ?")
+    label = tk.Label(root, text="Can not find adb.exe should i download it ?")
     label.pack(side="top", fill="both", expand=True, padx=20, pady=20)
 
     button_label = tk.Frame(root)
     button_label.pack(side="bottom", ipadx=10,ipady=10)
-    yes = tk.Button(button_label, text="Yes",width=20, command=lambda: root.destroy())
+    yes = tk.Button(button_label, text="Yes",width=20, command=lambda: get_zip())
     yes.pack(side="left", fill="none", expand=True, padx=2, pady=10 )
 
     no = tk.Button(button_label, text="no",width=20 , command=lambda: root.destroy())
@@ -35,6 +38,7 @@ def show_questnion():
 
     root.mainloop()
 
-show_questnion()
-#exist("adb.exe")
+
+
+exist("adb.exe")
 
