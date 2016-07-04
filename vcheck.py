2e0ef89eb9963cb9b0c60a90f1571c23a3037283
MrChaosbude <mail@chaosbude.com>
2016-07-04 18:21:53 +0200

    working on update check again

--- vcheck.py
+++ vcheck.py
@@ -21,27 +21,43 @@ def vcheck():
     urlValue = url.text
 
     if currentVersionValue != version:
-        print (messageValue)
-        print (urlValue)
-        dialog(messageValue,urlValue)
+        dialog(messageValue,urlValue, status=True)
+    else:
+        dialog(status=False)
 
-def dialog(masage,url):
+def callback(event):
+    webbrowser.open_new(event.widget.cget("text"))
+
+def dialog(masage,url,status=None):
     global root
     root = tk.Tk()
     root.title("Update")
+    if status == True:
+        label = tk.Label(root, text=masage)
+        label.pack(side="top", fill="both", expand=True, padx=20, pady=20)
 
-    label = tk.Label(root, text=masage)
-    label.pack(side="top", fill="both", expand=True, padx=20, pady=20)
-    label_1 = tk.Label(root, text=url)
-    label_1.pack(side="top", fill="both", expand=True, padx=20, pady=20)
+        lbl = tk.Label(root, text=url, fg="blue", cursor="hand2")
+        lbl.pack()
+        lbl.bind("<Button-1>", callback)
+        button_label = tk.Frame(root)
+        button_label.pack(side="bottom", ipadx=10, ipady=10)
+        yes = tk.Button(button_label, text="Update", width=20,
+                        command=lambda: webbrowser.open(url, new=0, autoraise=True))
+        yes.pack(side="left", fill="none", expand=True, padx=2, pady=10)
 
-    button_label = tk.Frame(root)
-    button_label.pack(side="bottom", ipadx=10,ipady=10)
-    yes = tk.Button(button_label, text="Update",width=20, command=lambda: webbrowser.open(url, new=0, autoraise=True))
-    yes.pack(side="left", fill="none", expand=True, padx=2, pady=10 )
+        no = tk.Button(button_label, text="No", width=20, command=lambda: root.destroy())
+        no.pack(side="right", fill="none", expand=True, padx=2, pady=10)
+    else:
+        label = tk.Label(root, text="You are already running the most up to date version!")
+        label.pack(side="top", fill="both", expand=True, padx=20, pady=20)
 
-    no = tk.Button(button_label, text="No",width=20 , command=lambda: root.destroy())
-    no.pack(side="right", fill="none", expand=True, padx=2, pady=10)
+        no = tk.Button(root, text="No", width=20, command=lambda: root.destroy())
+        no.pack( fill="none", expand=True, padx=2, pady=10)
+
+    separator = tk.Frame(root, height=2, bd=1, relief=tk.SUNKEN)
+    separator.pack(fill=tk.X, padx=5, pady=5)
+    donot = tk.IntVar()
+    tk.Checkbutton(root, text="Don't show this message again!", variable=donot).pack(side="left",padx=2, pady=2)
 
     root.mainloop()
 
